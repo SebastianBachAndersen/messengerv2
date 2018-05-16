d3d74ed0a05ae5b22f3c9ada8450a63477ec68bf
dog <ulri1122@gmail.com>
2018-05-16 12:44:11 +0200

    First class ever made (profile)

--- Messenger/Messenger/Profile.swift
+++ Messenger/Messenger/Profile.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+class Profile{
+    var name : String
+    var age : Int
+    var dateOfBirth: String
+    var relationshipStatus: String
+    var eMail: String
+    var friendList: Array<String>
+    var chat: Array<String>
+    var chatList: Array <String>
+    
+    init(name: String, age: Int, dateOfBrith : String, relationsshipStatus: String, eMail: String, friendList: Array<String>, chat : Array<String>, chatList: Array<String>){
+        self.name = name
+        self.age = age
+        self.dateOfBirth = dateOfBrith
+        self.relationshipStatus = relationsshipStatus
+        self.eMail = eMail
+        self.friendList = friendList
+        self.chat = chat
+        self.chatList = chatList
+    }
+    
+}
+
